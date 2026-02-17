23b6bb07b7224bfc0a203ebdce8de39ae4b85c7f
FranciscoJManasAlvarez <fma527@ual.es>
2026-02-17 17:33:03 +0100

    Ficheros de ejemplo para nodos y topics con QoS

---
+++ basic_python_pkg/basic_python_pkg/aux_topics_services.py
@@ -0,0 +1,56 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String, UInt16MultiArray, Float64, Float64MultiArray, MultiArrayDimension
+from basic_cpp_pkg.msg import Actuators
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSDurabilityPolicy, QoSHistoryPolicy
+
+
+class CurseNode(Node):
+    def __init__(self):
+        super().__init__('demo_node_py')
+        # Params
+        self.declare_parameter('example_param', 'value')
+
+        # Publisher
+        self.publisher_example_ = self.create_publisher(String,'/status', 10)
+        
+        # Subscription
+        qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.RELIABLE,         # o BEST_EFFORT
+            durability=QoSDurabilityPolicy.VOLATILE,           # o TRANSIENT_LOCAL
+            history=QoSHistoryPolicy.KEEP_LAST,                # o KEEP_ALL
+            depth=10                                            # tamaño del buffer
+        )
+        self.sub_example = self.create_subscription(Actuators, '/actuators', self.actuators_callback, qos_profile)
+        
+        self.initialize()
+    
+    def initialize(self):
+        self.get_logger().info('CurseExample::inicialize() ok.')
+        self.i = 1
+        self.timer = self.create_timer(10, self.iterate)
+
+    def actuators_callback(self, msg):
+        id = msg.id
+        angles = msg.angles
+        self.get_logger().info('%s: %.2f : %.1f : %.3f' % (id, angles[0], angles[1], angles[2]))
+
+    def iterate(self):
+        data = 'Mensaje '+str(self.i)
+        self.get_logger().warn('%s' % data)
+        self.i += 1
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    basic_node = CurseNode()
+    rclpy.spin(basic_node)
+
+    basic_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ basic_python_pkg/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+package_name = 'basic_python_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Francisco Jose Manas Alvarez',
+    maintainer_email='fjmanas@dia.uned.es',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'basic_node = basic_python_pkg.basic_node:main',
+            'demo_node = basic_python_pkg.demo_node:main',
+            'test_topics_services = basic_python_pkg.test_topics_services:main',
+            'aux_topics_services = basic_python_pkg.aux_topics_services:main'
+        ],
+    },
+)
